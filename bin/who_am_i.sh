bacc965892b4c31b82bb7bd511e85ba56f67d83d
SibongudumoJHB <linies369@gmail.com>
2020-07-08 12:21:29 +0200

    second commit

---
+++ bin/who_am_i.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo Kerberos ticket
+echo issued Jul  8 11:48:08 2020
+echo Expires ul  8 21:48:08 2020 #wanted to add some comment
+echo My Name Is Sibongudumo Ntombela #full name
+echo UserName Sintombe #my wethinkcode username
